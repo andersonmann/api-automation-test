930e9c6e3f05ab863e4cb424095a41a562b58bfd
andersonmann <andersonmann@notebook>
2019-07-08 20:26:47 -0300

    using matchers

--- api-automation-test/src/main/java/br/rs/ammann/rest/HelloWorldTest.java
+++ api-automation-test/src/main/java/br/rs/ammann/rest/HelloWorldTest.java
@@ -3,13 +3,20 @@
  */
 package br.rs.ammann.rest;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.request;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+
+import org.hamcrest.Matchers;
+import org.hamcrest.core.IsNot;
 import org.junit.Test;
 
-import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
@@ -37,10 +44,29 @@ public class HelloWorldTest {
 		validacao.statusCode(200);
 		get("http://restapi.wcaquino.me/ola").then().statusCode(200);
 
-		given() 
-		.when() 
-			.get("http://restapi.wcaquino.me/ola")
-		.then()
-			.statusCode(201);
+		given().
+		when().
+			get("http://restapi.wcaquino.me/ola").
+		then().
+			statusCode(200);
 	}
+
+	@Test
+	public void devoConhecerMatchersHamcrest() {
+		assertThat("Maria", Matchers.is("Maria"));
+		assertThat(128, Matchers.isA(Integer.class));
+		assertThat(128, Matchers.greaterThan(120));
+		
+		java.util.List<Integer> impares = Arrays.asList(1,3,5,7,9);
+		assertThat(impares, hasSize(5));
+		assertThat(impares, contains(1,3,5,7,9));
+		assertThat(impares, hasItem(9));
+		assertThat(impares, hasItems(9,5));
+		assertThat(impares, containsInAnyOrder(9,5,1,7,3));
+		
+		assertThat("Maria", is(not("João")));
+		assertThat("Maria", not("João"));
+		assertThat("Maria", anyOf(is("Maria"),is("Joaquina")));
+	}
+
 }
