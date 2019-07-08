0f787d93161474227dff4ef4ed5cfa714eabbd5c
andersonmann <andersonmann@notebook>
2019-07-08 19:04:54 -0300

    added fluent mode validation

--- api-automation-test/src/main/java/br/rs/ammann/rest/HelloWorldTest.java
+++ api-automation-test/src/main/java/br/rs/ammann/rest/HelloWorldTest.java
@@ -3,7 +3,16 @@
  */
 package br.rs.ammann.rest;
 
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 /**
  * @author andersonmann
@@ -13,7 +22,25 @@ public class HelloWorldTest {
 
 	@Test
 	public void testHelloWorld() {
-		
+		Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
+		assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+		assertTrue(response.getStatusCode() == 200);
+		assertEquals(200, response.getStatusCode());
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
 	}
 
+	@Test
+	public void devoConhecerOutrasFormasRestAssured() {
+		Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+		get("http://restapi.wcaquino.me/ola").then().statusCode(200);
+
+		given() 
+		.when() 
+			.get("http://restapi.wcaquino.me/ola")
+		.then()
+			.statusCode(201);
+	}
 }
