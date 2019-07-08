25c34aa6e83b727b39862c12699e410e01372e7d
andersonmann <andersonmann@notebook>
2019-07-08 16:59:17 -0300

    created test class

---
+++ api-automation-test/src/main/java/br/rs/ammann/rest/HelloWorldTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.rs.ammann.rest;
+
+import org.junit.Test;
+
+/**
+ * @author andersonmann
+ *
+ */
+public class HelloWorldTest {
+
+	@Test
+	public void testHelloWorld() {
+		
+	}
+
+}
